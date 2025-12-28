8ad8706ed465f87ef5db750c74f624f800d384d5
mashnts <mashentsovaalina@gmail.com>
2025-12-28 21:36:32 +0200

    корзина

--- shop/settings.py
+++ shop/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'main',
+    'main', 'cart',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +110,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CART_SESSION_ID = 'cart'
